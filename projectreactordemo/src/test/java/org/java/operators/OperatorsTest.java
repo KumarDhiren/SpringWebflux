075e8972ea152de41baabb356248e44c02336228
Dhiren <dhiren.patra@outlook.com>
2019-12-27 18:25:31 +0530

    operator in reactive programming

--- projectreactordemo/src/test/java/org/java/operators/OperatorsTest.java
+++ projectreactordemo/src/test/java/org/java/operators/OperatorsTest.java
@@ -3,6 +3,8 @@ package org.java.operators;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
 public class OperatorsTest {
 
     @Test
@@ -17,6 +19,90 @@ public class OperatorsTest {
         Flux.range(1,5)
                 .flatMap(i -> Flux.range(i * 10 , 3))
                 .subscribe(System.out::println);
+    }
+
+    @Test
+    public void concat() throws InterruptedException {
+        final Flux<Integer> first = Flux.range(1, 6)
+                .delayElements(Duration.ofSeconds(1));
+        final Flux<Integer> second = Flux.range(6, 5)
+                .delayElements(Duration.ofSeconds(1));
+
+        Flux.concat(first,second)
+                .subscribe(System.out::println);
+
+        Thread.sleep(12000);
+
+    }
+
+    @Test
+    public void concatWith() throws InterruptedException {
+        final Flux<Integer> first = Flux.range(1, 5)
+                .delayElements(Duration.ofSeconds(1));
+        final Flux<Integer> second = Flux.range(6, 5)
+                .delayElements(Duration.ofSeconds(1));
+
+        first.concatWith(second)
+                .subscribe(System.out::println);
+
+        Thread.sleep(12000);
+
+    }
+
+    @Test
+    public void merge() throws InterruptedException {
+        final Flux<Integer> first = Flux.range(1, 5)
+                .delayElements(Duration.ofSeconds(1));
+        final Flux<Integer> second = Flux.range(6, 5)
+                .delayElements(Duration.ofSeconds(1));
+
+        Flux.merge(first,second)
+                .subscribe(System.out::println);
+
+        Thread.sleep(6000);
+
+    }
+
+    @Test
+    public void mergeWith() throws InterruptedException {
+        final Flux<Integer> first = Flux.range(1, 6)
+                .delayElements(Duration.ofSeconds(1));
+        final Flux<Integer> second = Flux.range(6, 5)
+                .delayElements(Duration.ofSeconds(1));
+
+        first.mergeWith(second)
+                .subscribe(System.out::println);
+
+        Thread.sleep(6000);
+
+    }
+
+    @Test
+    public void zip() throws InterruptedException {
+        final Flux<Integer> first = Flux.range(1, 6)
+                .delayElements(Duration.ofSeconds(1));
+        final Flux<Integer> second = Flux.range(6, 5)
+                .delayElements(Duration.ofSeconds(1));
+
+        Flux.zip(first,second)
+                .subscribe(System.out::println);
+
+        Thread.sleep(6000);
+
+    }
+
+    @Test
+    public void zipWith() throws InterruptedException {
+        final Flux<Integer> first = Flux.range(1, 6)
+                .delayElements(Duration.ofSeconds(1));
+        final Flux<Integer> second = Flux.range(6, 5)
+                .delayElements(Duration.ofSeconds(1));
+
+        first.zipWith(second)
+                .subscribe(System.out::println);
+
+        Thread.sleep(6000);
+
     }
 
 }
